1b6ee4748f62016305f0207a72c2180f84583218
thunderteammax <94621146+thunderteammax@users.noreply.github.com>
2021-12-11 22:44:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,4 +1,4 @@
-# Author: Fayas (https://github.com/FayasNoushad) (@FayasNoushad)
+# Athor: Fayas (https://github.com/FayasNoushad) (@FayasNoushad)
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
